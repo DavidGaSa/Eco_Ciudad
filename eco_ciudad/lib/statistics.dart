e5331e8bcffed1fd9d04cb6839f69dd0926b4180
garciasalgadodavid4 <garciasalgadodavid4@gmail.com>
2025-09-23 17:29:24 +0000

    Estadísticas 1

--- eco_ciudad/lib/statistics.dart
+++ eco_ciudad/lib/statistics.dart
@@ -1,13 +1,64 @@
 import 'package:flutter/material.dart';
 
-class StatisticsPage extends StatelessWidget {
+class StatisticsPage extends StatefulWidget {
   const StatisticsPage({super.key});
+
+  @override
+  State<StatisticsPage> createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  int _recycleCount = 0;
+
+  void _incrementRecycleCount() {
+    setState(() {
+      _recycleCount++;
+    });
+  }
+
+  void _decrementRecycleCount() {
+    setState(() {
+      if (_recycleCount > 0) {
+        _recycleCount--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mis Estadísticas')),
-      body: const Center(child: Text('Aquí van las estadísticas del usuario')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Bolsas recicladas este mes:',
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              '$_recycleCount',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _decrementRecycleCount,
+            tooltip: '-1 Bolsa Reciclada',
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _incrementRecycleCount,
+            tooltip: '+1 Bolsa Reciclada',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
     );
   }
 }
